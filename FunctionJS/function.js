a76ccd5920e73a1a001850dad4e4e15da2cc3860
youngdreamer33 <b92.a75@gmail.com>
2017-07-17 21:10:18 +0200

    Cours sur les fonctions JS d'OC / Mise en pratique des apprentissages

--- FunctionJS/function.js
+++ FunctionJS/function.js
@@ -1,3 +1,34 @@
-/**
- * Created by bapti on 16/07/2017.
- */
+var Album = {
+    init: function (artist, name, year) {
+        this.artist= artist;
+        this.name=name;
+        this.year=year;
+    },
+    desc: function () {
+        var show= this.name+" by "+this.artist+" ( "+this.year+" ).";
+        return show;
+    }
+};
+
+var Albums = [];
+
+var alb1 = Object.create(Album);
+alb1.init("Kanye West", "My Beautiful Dark Twisted Fantasy", "2010");
+
+var alb2 = Object.create(Album);
+alb2.init("Kanye West", "Yeezus", "2013");
+
+var alb3 = Object.create(Album);
+alb3.init("Kanye West","The Life Of Pablo", "2016");
+
+var alb4 = Object.create(Album);
+alb4.init("Kanye West", "The College Dropout", "2004");
+
+Albums.push(alb1);
+Albums.push(alb2);
+Albums.push(alb3);
+Albums.push(alb4);
+
+Albums.forEach(function (Album){
+    console.log(Album.desc());
+});
